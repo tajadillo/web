562a5a66dc03cdd9d417274d047a21e42782dc5d
Tajadillo <91347298+tajadillo@users.noreply.github.com>
2024-02-21 03:53:27 -0300

    Add files via upload

--- menu/formulario.php
+++ menu/formulario.php
@@ -6,23 +6,4 @@ $mensaje = $_POST["mensaje"];
 $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje
     mail($destino, "Contacto",  $contenido);
 header("Location:gracias.html");
-
-if(isset($_POST['submit'])) {
-    if(empty($nombre)) {
-        echo "<p class='error'>* Agrega tu nombre </p>";
-    }
-    else {
-        if(strlen(nombre) > 15) {
-            echo "<p class='error'>* El nombre es muy largo </p>";
-        }
-    }
-    if(empty($correo)) {
-        echo "<p class='error'>* Agrega tu correo </p>";
-    }
-    else {
-        if(!filter_var($correo, FILTER_VALIDATE_EMAL)) {
-            echo "<p class='error'>* El corro es incorrecto </p>";
-        }
-    }
-}
 ?>
