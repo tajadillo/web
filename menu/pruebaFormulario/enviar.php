53a12dd8898046719f562cbd131ec036e684e16f
Tajadillo <91347298+tajadillo@users.noreply.github.com>
2024-03-07 17:57:51 -0300

    Update enviar.php

--- menu/pruebaFormulario/enviar.php
+++ menu/pruebaFormulario/enviar.php
@@ -1,9 +1,9 @@
 <?
-$destino = "richard_tajadillo@outlook.com";
-$Nombre = $_POST["Nombre"];
-$Correo = $_POST["Correo"];
-$Telefono = $_POST["Telefono"];
-$Mensaje = $_POST["Mensaje"];
+$destino = "richard.tajadillo@gmail.com";
+$Nombre = $_post["Nombre"];
+$Correo = $_post["Correo"];
+$Telefono = $_post["Telefono"];
+$Mensaje = $_post["Mensaje"];
 $contenido = "Nombre: " .$Nombre . "\nCorreo: " . $Correo . "\nTeléfono: " . $Telefono . "\nMensaje: " . $Mensaje;
 mail($destino, "Contacto", $contenido);
 header("Location:gracias.html")
