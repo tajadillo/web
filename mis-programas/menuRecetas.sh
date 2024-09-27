afce68ee760f70ad0962768c959ef218af255dcd
Tajadillo <91347298+tajadillo@users.noreply.github.com>
2024-09-27 03:43:48 -0300

    Add files via upload

--- mis-programas/menuRecetas.sh
+++ mis-programas/menuRecetas.sh
@@ -1,27 +1,116 @@
 #!/bin/bash
+#Autor: Richard Tajadillo
+#Comando para buscar archivos y directorios
+# LICENCIA GNU GPL 3.0
+
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 #Aquí tienes un script en Bash que realiza los cálculos que has solicitado. El script permite al usuario agregar ingredientes con su costo al por mayor, calcular el costo según la receta y determinar el precio de venta al público.
 
 #Script en Bash
 #bash
 
+
+
+
+
+
+
+
+
+#############################
+function ctrl_c(){
+    clear
+    sleep 1
+    echo -e "\n ${yellowColour}Terminamos...${endColour} \n\n"
+    tput cnorm
+    exit 1
+}
+
+trap ctrl_c SIGINT
+
+##############################
+
+
+#saludo
+
+function saludar(){
+# Obtener la hora actual
+hora_actual=$(date +'%H')
+
+# Determinar el saludo basado en la hora
+if [ $hora_actual -ge 5 ] && [ $hora_actual -lt 12 ]; then
+    saludo="¡Buenos días!"
+elif [ $hora_actual -ge 12 ] && [ $hora_actual -lt 20 ]; then
+    saludo="¡Buenas tardes!"
+else
+    saludo="¡Buenas noches!"
+fi
+
+# Mostrar el saludo
+echo $saludo
+}
+
+
+
+
+tput civis
+
+clear
+sleep 1
+
+echo -e "\n hola"
+echo
+sleep 2
+
+tput cnorm
+read -p "cual es tu nombre? " usuario
+
+sleep 2
+clear
+sleep 2
+tput civis
+
+echo -e "\n ${redColour}############################################################################### ${endColour}"
+echo
+echo
+saludar
+echo
+echo
+echo -e "\n ${yellowColour}Correcto${endColour} ${greenColour} $usuario ${endColour} ${yellowColour}Con este programa sacaremos el costo de tu menu. ${endColour}"
+echo -e "\n ${redColour}############################################################################### ${endColour}"
+sleep 4
+clear
+#fin saludo
+
 # Función para mostrar el menú de opciones
 mostrar_menu() {
-    echo "Seleccione una opción:"
-    echo "1) Agregar ingrediente"
-    echo "2) Calcular costo de la receta"
-    echo "3) Salir"
+    echo -e "${blueColour}Seleccione una opción:${endColour}"
+    echo -e "${yellowColour}1) Agregar ingrediente${endColour}"
+    echo -e "${yellowColour}2) Calcular costo de la receta${endColour}"
+    echo -e "${yellowColour}3) Salir${endColour}"
 }
 
 # Inicializar variables
 declare -A ingredientes
 total_costo=0
 
+    echo -e "\n\t ${greenColour}consultar tabla de equivalencias para tener las medidas en claridad${endColour} \n\n"
+
 # Ciclo principal
 while true; do
     mostrar_menu
+    echo
     read -p "Opción: " opcion
-
-    echo -e "\n\t consultar tabla de equivalencias para tener las medidas en claridad \n\n"
 
     case $opcion in
         1)
@@ -29,7 +118,7 @@ while true; do
             read -p "Costo por kilo (o unidad): " costo
             read -p "Cantidad en la receta (kilos o unidades): " cantidad
             ingredientes["$nombre"]="$costo:$cantidad"
-            echo "Ingrediente $nombre agregado."
+            echo -e "\n \t ${greenColour}Ingrediente${endColour} ${redColour}$nombre${endColour} ${greenColour}agregado.${endColour}\n"
             ;;
 
         2)
@@ -39,7 +128,7 @@ while true; do
                 costo_info=(${ingredientes["$ingrediente"]//:/ })
                 costo=${costo_info[0]}
                 cantidad=${costo_info[1]}
-                costo_total=$(echo "$costo * $cantidad" | bc)
+                costo_total=$(echo "$costo / $cantidad" | bc)
                 total_costo=$(echo "$total_costo + $costo_total" | bc)
                 echo "$ingrediente: Costo total = $costo_total"
             done
@@ -48,7 +137,7 @@ while true; do
             read -p "Número de personas: " num_personas
 
             precio_venta=$(echo "$total_costo * $factor / $num_personas" | bc)
-            echo "Precio de venta al público por plato: $precio_venta"
+            echo "Precio de venta al público por plato: $precio_venta'00'"
             ;;
 
         3)
