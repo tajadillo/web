f52c43edf658aed26454b47be0e36b667c87f28e
Tajadillo <91347298+tajadillo@users.noreply.github.com>
2024-09-30 04:25:41 -0300

    Add files via upload

--- mis-programas/menuRecetas.sh
+++ mis-programas/menuRecetas.sh
@@ -3,6 +3,8 @@
 #Comando para buscar archivos y directorios
 # LICENCIA GNU GPL 3.0
 
+#Aquí tienes un script en Bash que realiza los cálculos que has solicitado. El script permite al usuario agregar ingredientes con su costo al por mayor, calcular el costo según la receta y determinar el precio de venta al público.
+
 
 #Colours
 greenColour="\e[0;32m\033[1m"
@@ -13,15 +15,20 @@ yellowColour="\e[0;33m\033[1m"
 purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
+blackColour="\e[0;30m\033[1m"
+whiteColour="\e[0;97m\033[1m"
+darkGrayColour="\e[0;90m\033[1m"
+#Esto cubre una amplia gama de colores ANSI que puedes usar para formatear texto en la terminal. ¡Espero que te sea útil!
 
-#Aquí tienes un script en Bash que realiza los cálculos que has solicitado. El script permite al usuario agregar ingredientes con su costo al por mayor, calcular el costo según la receta y determinar el precio de venta al público.
-
-#La mejor manera (y la más logica) para describirle una receta al cliente seria.
-#1. Describir cual es la proteina o el ingrediente principal
-#2. Cual es el metodo de cocción de dicho ingrediente
-#3. Describir cual es la salsa o acompañante
-#4. Cual es el metodo de preparación de dicho acompañante (de no conocer cual es el metodo de preparacion se puede saltar este paso)
-#5. Describir cuales son las guarniciones para el ingrediente principal
+#################################################################################################
+#La mejor manera (y la más logica) para describirle una receta al cliente seria.                #
+#1. Describir cual es la proteina o el ingrediente principal                                    #
+#2. Cual es el metodo de cocción de dicho ingrediente                                           #
+#3. Describir cual es la salsa o acompañante                                                    #
+#4. Cual es el metodo de preparación de dicho acompañante                                       #
+#       (de no conocer cual es el metodo de preparacion se puede saltar este paso)              #
+#5. Describir cuales son las guarniciones para el ingrediente principal                         #
+#################################################################################################
 
 
 
@@ -58,9 +65,6 @@ fi
 echo $saludo
 }
 
-
-
-
 tput civis
 
 clear
@@ -91,12 +95,26 @@ clear
 #fin saludo
 
 # Función para mostrar el menú de opciones
-mostrar_menu() {
+function mostrar_menu() {
     echo -e "${blueColour}Seleccione una opción:${endColour}"
     echo -e "${yellowColour}1) Agregar ingrediente${endColour}"
     echo -e "${yellowColour}2) Calcular costo de la receta${endColour}"
-    echo -e "${yellowColour}3) Salir${endColour}"
+    echo -e "${yellowColour}3) Ayuda${endColour}"
+    echo -e "${yellowColour}4) Salir${endColour}"
 }
+#Fin menú
+
+# Panel de ayuda
+function help_panel(){
+    #Uso del script:
+    echo -e "\n ${blackColour}Agrega los ingredientes por su nombre, según la receta.${endColour}"
+    echo -e "\n ${blackColour}Luego ingresarás cuanto te costó comprar cada producto al por mayor con tu proveedor.${endColour}"
+    echo -e "\n ${blackColour}Seguidamente la cantidad que te pide la receta.${endColour}"
+    echo -e "\n ${blackColour}Es de suma importancia consultar la tabla de equivalencias para tener las medidas en claridad${endColour}"
+    echo -e "\n ${blackColour}¡Espero que te sea útil! Si tienes alguna pregunta o necesitas más modificaciones, no dudes en decírmelo.${endColour}\n"
+}
+#Fin ayuda
+
 
 # Inicializar variables
 declare -A ingredientes
@@ -139,6 +157,11 @@ while true; do
             ;;
 
         3)
+            sleep 1
+            clear
+            help_panel
+            ;;
+        4)
             echo "Saliendo..."
             exit 0
             ;;
@@ -149,8 +172,3 @@ while true; do
     esac
 done
 
-#Uso del script:
-#Agregar ingredientes: Puedes añadir tantos ingredientes como desees, especificando su costo y la cantidad utilizada en la receta.
-#Calcular costo de la receta: Muestra el costo total de los ingredientes, solicita un factor de multiplicación y el número de personas, y calcula el precio de venta por plato.
-#Salir: Termina la ejecución del script.
-#¡Espero que te sea útil! Si tienes alguna pregunta o necesitas más modificaciones, no dudes en decírmelo.
---
+++ mis-programas/promedioEscuela.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+#Autor: Richard Tajadillo
+#Comando para buscar archivos y directorios
+# LICENCIA GNU GPL 3.0
+
+#Aquí tienes un script en Bash que realiza los cálculos que has solicitado. El script permite al usuario agregar ingredientes con su costo al por mayor, calcular el costo según la receta y determinar el precio de venta al público.
+
+
+#Colours
+rosa="\e[0;41m\101[1m"
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+blackColour="\e[0;30m\033[1m"
+whiteColour="\e[0;97m\033[1m"
+darkGrayColour="\e[0;90m\033[1m"
+naranja="\033[38;2;243;134;48mgiant goldfish\033[0m"
+#Esto cubre una amplia gama de colores ANSI que puedes usar para formatear texto en la terminal. ¡Espero que te sea útil!
+
+#############################
+function ctrl_c(){
+    clear
+    sleep 1
+    echo -e "\n ${yellowColour}Terminamos...${endColour} \n\n"
+    tput cnorm
+    exit 1
+}
+
+trap ctrl_c SIGINT
+
+##############################
+
+
+#saludo
+
+function saludar(){
+    # Obtener la hora actual
+    hora_actual=$(date +'%H')
+
+# Determinar el saludo basado en la hora
+if [ $hora_actual -ge 5 ] && [ $hora_actual -lt 12 ]; then
+    saludo="¡Buenos días!"
+elif [ $hora_actual -ge 12 ] && [ $hora_actual -lt 20 ]; then
+    saludo="¡Buenas tardes!"
+else
+    saludo="¡Buenas noches!"
+fi
+
+# Mostrar el saludo
+echo $saludo
+}
+
+tput civis
+
+clear
+sleep 1
+
+echo -e "\n hola"
+echo
+sleep 2
+
+tput cnorm
+read -p "cual es tu nombre? " usuario
+
+sleep 2
+clear
+sleep 2
+tput civis
+
+
+
+echo -e "\033[94;105mTexto en azul brillante con fondo magenta brillante\033[0m
+"
+
+echo -e "\n ${redColour}############################################################################### ${endColour}"
+echo
+echo
+saludar
+echo -e "\n ${yellowColour}Correcto${endColour} ${greenColour} $usuario ${endColour} ${yellowColour}Elige que materia quieres llevar, y después pon tus 4 calificaciones del semestre.\n Con base en esas calificaciones obtendrás tu promedio. ${endColour}"
+echo -e "\n ${redColour}############################################################################### ${endColour}"
+sleep 15
+clear
+#fin saludo
+
+# Función para mostrar el menú de opciones
+function mostrar_menu() {
+    echo -e "${blueColour}Seleccione una opción:${endColour}"
+    echo -e "${yellowColour}1) Agregar ingrediente${endColour}"
+    echo -e "${yellowColour}2) Calcular costo de la receta${endColour}"
+    echo -e "${yellowColour}3) Ayuda${endColour}"
+    echo -e "${yellowColour}4) Salir${endColour}"
+}
+#Fin menú
+
+# Panel de ayuda
+function help_panel(){
+    #Uso del script:
+    echo -e "\n ${blackColour}.${endColour}"
+    echo -e "\n ${blackColour}.${endColour}"
+    echo -e "\n ${blackColour}.${endColour}"
+    echo -e "\n ${blackColour}.${endColour}"
+    echo -e "\n ${blackColour}.${endColour}\n"
+}
+#Fin ayuda
+
+
+#calculadora de promedio
+echo -e "${redColour}CALIFICACIONES DE LA ACADEMIA${endColour} \n\n${blueColour}ASIGNATURAS:${endColour}"
+echo -e "${greenColour}1) Coreografía \n2) Expresión Corporal \n3) Condición Física \n4) Compañerismo \n5) Disciplina${endColour}"
+read -p "Elige solo uno: " s1
+if [ $s1 == 1 ]; then
+    asignatura="Coreografía"
+    echo "Tú elegiste $asignatura"
+elif [ $s1 == 2 ]; then
+    asignatura="Expresión Corporal"
+    echo "Tú elegiste $asignatura"
+elif [ $s1 == 3 ]; then
+    asignatura="Condición Física"
+    echo "Tú elegiste $asignatura"
+elif [ $s1 == 4 ]; then
+    asignatura="Compañerismo"
+    echo "Tú elegiste $asignatur"
+elif [ $s1 == 5 ]; then
+    asignatura="Disciplina"
+    echo "Tú elegiste $asignaturea"
+else
+    sleep 2
+    clear
+    echo -e "\n ${redColour}El ramo elegido no es valido${endColour}"
+    echo -e "\n ${yellowColour}Terminamos $usuario...${endColour} \n"
+    sleep 2
+    exit
+fi
+
+read -p "$signature primer bloque: " f4
+read -p "$signature segundo bloque: " s4
+read -p "$signature tercer bloque: " t4
+read -p "$signature cuarto bloque: " f4
+
+echo -e "\nCALIFICACIONES"
+p1=$(( f4 + s4 ))
+let p1/=2
+echo "Primer partial: $p1"
+p2=$(( t4 + f4 ))
+let p2/=2
+echo "Segundo partial: $p2"
+promedio=$(( p1 +p2 ))
+let promedio/=2
+echo "Promedio del semestre en $asignatura: $promedio"
+#fin calculadora
