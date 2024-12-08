56e89e746bcd6100755f648397c50ff75b25a030
Tajadillo <91347298+tajadillo@users.noreply.github.com>
2024-12-08 03:58:13 -0300

    Delete mis-programas directory

--- mis-programas/computacion/prometeo/documentosDmenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#https://www.youtube.com/@Tuxer76/videos
-
-DOCS=$(find /media/tajadillo/TAJADILLO/documentos/ -name "*.txt" | less | dmenu -l 10 -i -p "Cual Manual Quieres Ver?")
-PROGRAM=nvim
-kitty --execute $PROGRAM "$DOCS"
-
